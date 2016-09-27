0c8f1624e2aac91e0ef003c13680d77169617f00
dcuda <dcuda@cisco.com>
2016-09-27 08:31:32 +0200

    dfs, read from file

--- ch19/Graph.hpp
+++ ch19/Graph.hpp
@@ -5,6 +5,9 @@
 #include <unordered_set>
 #include <memory>
 #include <typeinfo>
+#include <fstream>
+#include <sstream>
+#include "../utils.hpp"
 
 
 template <class T>
@@ -52,6 +55,12 @@ class GraphNode {
     bool is_visited = false;
     void swap(GraphNode<T>& other);
     public:
+    void reset_stats () {
+        pre_visit_time = 0;
+        visit_time = 0;
+        post_visit_time = 0;
+        is_visited = false;
+    }
     GraphNode () : data(nullptr) {}
     GraphNode (const std::shared_ptr<T>& data_ptr) : data(data_ptr) {}
     GraphNode (const GraphNode<T>& other);
@@ -62,7 +71,6 @@ class GraphNode {
     const int get_post_visit_time () const { return post_visit_time; }
     const bool get_is_visited () const { return is_visited; }
     const std::shared_ptr<T> get_data() const {return data;}
-
     void set_pre_visit_time (int t) { pre_visit_time = t; }
     void set_visit_time (int t) { visit_time = t; }
     void set_post_visit_time (int t) { post_visit_time = t; }
@@ -168,10 +176,15 @@ class Graph {
         std::unordered_map<T, std::shared_ptr<GraphNode<T>> > nodes;
         std::unordered_map<std::pair<T,T>, std::shared_ptr<Edge<T>>,
             KeyHashPair<T,T>> edges;
+        void reset_nodes_status();
+        template <class U>
+        void explore(std::shared_ptr<GraphNode<T>>& curr, int& time, U u);
     public:
         Graph() {};
+        void load_from_file (const std::string& filename);
         void add_edge(const std::shared_ptr<T> from, 
                 const std::shared_ptr<T> to, const int weigth = 1);
+        void dfs ();
 
     friend std::ostream& operator<< (std::ostream& out, const Graph<T>& g) {
         for (const auto& node_pair: g.nodes) {
@@ -205,9 +218,79 @@ void Graph<T>::add_edge(const std::shared_ptr<T> from,
     }
     edge->weights.emplace_back(weight);
     source->adj_list.emplace_back(edge);
-    //destination->adj_list.emplace_back(source);
 }
 
 
+template <class T>
+void Graph<T>::reset_nodes_status()
+{
+    for (auto& it: nodes) {
+        auto node = it.second;
+        node->reset_stats();
+    }
+}
 
+
+template <class T>
+template <class U>
+void Graph<T>::explore(std::shared_ptr<GraphNode<T>>& curr, int& time, U u)
+{
+    if (curr->is_visited) {
+        throw std::logic_error("Visiting an already visited node");
+    }
+    curr->pre_visit_time = time++;
+    curr->is_visited = true;
+    u(curr);
+    for (auto& edge: curr->adj_list) {
+        if (edge->from != curr) {
+            throw std::logic_error("source edge node differes from current node");
+        }
+        if (!edge->to->is_visited) {
+            explore(edge->to, time, u);
+        }
+    }
+    curr->post_visit_time = time++;
+}
+
+template <class T>
+void Graph<T>::dfs()
+{
+    auto time = 0;
+    reset_nodes_status();
+    for (auto& it: nodes) {
+        auto start = it.second;
+        try {
+            if(!start->is_visited) {
+                explore(start, time, [](auto node) {std::cout << *node;});
+            }
+            std::cout << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+}
+
+
+template <class T>
+void Graph<T>::load_from_file (const std::string& filename)
+{
+    std::ifstream input_file(filename);
+    if (!input_file.good()) {
+        std::cout << "Could not open file " << filename << std::endl;
+        return;
+    }
+    std::string line;
+    T a, b;
+    while (getline(input_file, line)) {
+        if (line.size() > 1) {
+            std::stringstream ss(line);
+            ss >> a; ss >> b;
+            {
+                auto a_node_ptr = std::make_shared<T>(a);
+                auto b_node_ptr = std::make_shared<T>(b);
+                add_edge(a_node_ptr, b_node_ptr);
+            }
+        }
+    }
+}
 
--- ch19/test.cpp
+++ ch19/test.cpp
@@ -7,18 +7,9 @@
 
 int main (void)
 {
-    auto from = std::make_shared<std::string>("A");
-    auto to   = std::make_shared<std::string>("B");
-    auto n1 = std::make_shared<GraphNode<std::string>>(from);
-    auto& n1_ref = n1;
-    auto n2 = std::make_shared<GraphNode<std::string>>(to);
-    std::vector<std::shared_ptr<GraphNode<std::string>>> nodes;
-    nodes.emplace_back(n1_ref);
-
     Graph<std::string> g;
-    g.add_edge(from, to);
+    g.load_from_file("graph.txt");
+    g.dfs();
     std::cout << g << std::endl;
-
-
     return 0;
 }
